149f0723c8e87c7febbe235b924bf4054071fcfe
VzxPLnHqr <VzxPLnHqr@proton.me>
2023-03-10 22:20:17 +0000

    CalicoConsole - broken

--- src/main/scala/fliptxo/Main.scala
+++ src/main/scala/fliptxo/Main.scala
@@ -5,8 +5,7 @@ import calico.html.io.{given,*}
 import cats.effect.*
 import fs2.dom.*
 
-object FliptxoApp extends IOWebApp:
+object Main extends IOWebApp:
   val hash = scoin.Crypto.sha256(scodec.bits.ByteVector("abc".getBytes)).toHex
   val pub = scoin.Crypto.PrivateKey(BigInt(5)).publicKey
-  def render: Resource[IO, HtmlElement[IO]] =
-    div(s"Fliptxo - From Alice to Bob to Alice to Bob to Alice ... N times, $hash, $pub")
+  def render: Resource[IO, HtmlElement[IO]] = CalicoConsole[IO].component
